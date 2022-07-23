aaa8ca5e4c98699eefb786a6d3942027e6cdb667
marjsky <67877013+marjsky@users.noreply.github.com>
2022-07-23 16:35:02 -0400

    update coding of imports and features

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import axios from 'axios';
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
-import Hancock from './hancock.png';
-import FridayNightLights from './friday_night_lights.png';
-import JustMercy from './just_mercy.png';
+import { RegistrationView } from '../registration-view/registration-view';
+
 
 export class MainView extends React.Component {
 
   constructor() {
     super();
+    // Initial state is set to null
     this.state = {
       movies: [],
-      selectedMovie: null
+      selectedMovie: null,
+      user: null,
+      register: null
     }
   }
 
@@ -28,18 +31,39 @@ export class MainView extends React.Component {
       });
   }
 
-  setSelectedMovie(newSelectedMovie) {
+/*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
+
+  setSelectedMovie(movie) {
     this.setState({
-      selectedMovie: newSelectedMovie
+      selectedMovie: movie
     });
   }
-  
+
+/* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
+  onRegistration(register) {
+    this.setState({
+      register
+    });
+  }
+
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user, register } = this.state;
 
-    if (selectedMovie) return <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { 
-      this.setSelectedMovie(newSelectedMovie); }}/>;
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
+    if (!register) return <RegistrationView onRegistration={register => this.onRegistration(register)} />;
+
+    if (selectedMovie) return <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => {this.setSelectedMovie(newSelectedMovie); }}/>;
+
+    // Before the movies have been loaded
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
