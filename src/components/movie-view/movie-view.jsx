21eabc904a418bd1d23fa48d8998cb5d34e69c2a
marjsky <67877013+marjsky@users.noreply.github.com>
2022-07-24 01:43:00 -0400

    add prop-types to movie view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 export class MovieView extends React.Component {
 
@@ -26,9 +27,32 @@ export class MovieView extends React.Component {
           <span className="label">Description: </span>
           <span className="value">{movie.Description}</span>
         </div>
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Driector: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
         <button onClick={() => { onBackClick(null); }}>Back</button>
 
       </div>
     );
   }
 }
+
+MovieView.PropTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    })
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired
+};
