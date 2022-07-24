acff11ec2e25fe6653304fc39862d5bc2151b1db
marjsky <67877013+marjsky@users.noreply.github.com>
2022-07-24 18:54:32 -0400

    add react bootstrap codes in login view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Container, Row, Col, Card, Form, Button, Navbar, Nav } from 'react-bootstrap';
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
@@ -14,17 +15,49 @@ export function LoginView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)}  />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+    <Container fluid className='loginContainer my-2 mx-12'>
+      <Navbar bg='primary' variant='dark'>
+        <Container>
+          <Navbar.Brand href='#home'>AppFlix</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link href='#Register'>Register</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body className='mt-3'>
+              <Card.Title style={{textAlign: 'center'}}>Login </Card.Title>
+              <Form>
+                <Form.Group controlId='formUsername'>
+                  <Form.Label>Username:</Form.Label>
+                  <Form.Control 
+                    type='text' 
+                    onChange={e => setUsername(e.target.value)} />
+                </Form.Group>
+
+                <Form.Group controlId='formPassword'>
+                  <Form.Label>Password:</Form.Label>
+                  <Form.Control 
+                    type='password' 
+                    onChange={e => setPassword(e.target.value)} />
+                </Form.Group>
+                <Button 
+                  variant='primary' 
+                  type='submit' 
+                  onClick={handleSubmit}>
+                  Submit
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
