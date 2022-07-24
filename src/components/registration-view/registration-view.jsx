41859ae1a80466e0ef94698126189a6cdc267f3c
marjsky <67877013+marjsky@users.noreply.github.com>
2022-07-24 18:54:51 -0400

    add react bootstrap codes in registration view

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import {Form, Button, Card, CardGroup, Container, Col, Row} from 'react-bootstrap';
 
 export function RegistrationView(props) {
   const [ username, setUsername ] = useState('');
@@ -14,25 +15,67 @@ export function RegistrationView(props) {
   };
 
   return(
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />;
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />;
-      </label>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />;
-      </label>
-      <label>
-        Birthday:
-        <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />;
-      </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+    <Container>
+      <Row>
+        <Col>
+          <CardGroup>
+            <Card>
+              <Card.Body>
+                <Card.Title>Please Register</Card.Title>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Username:</Form.Label>
+                      <Form.Control
+                        type="text" 
+                        value={username} 
+                        onChange={e => setUsername(e.target.value)}
+                        required 
+                        placeholder='Enter a username'
+                        />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Password:</Form.Label>
+                      <Form.Control 
+                        type="password" 
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)}
+                        required
+                        minLength={8}
+                        placeholder='Your password must be 8 or more characters' 
+                        />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Email:</Form.Label>
+                      <Form.Control
+                        type="email"
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}
+                        required
+                        placeholder='Enter your email address' 
+                        />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Birthday:</Form.Label>
+                      <Form.Control 
+                        type="date" 
+                        value={birthday} 
+                        onChange={e => setBirthday(e.target.value)} 
+                        />
+                  </Form.Group>
+                  <Button
+                    variant='primary'
+                    type="submit" 
+                    onClick={handleSubmit}>
+                      Submit
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Col>
+      </Row>
+    </Container>
+    
   );
 }
 
