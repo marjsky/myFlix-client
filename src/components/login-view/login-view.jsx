006b9d7bfa581594a0888e4f105443ef8ab566b6
marjsky <67877013+marjsky@users.noreply.github.com>
2022-07-28 23:07:22 -0400

    revise login-view propTypes

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -107,7 +107,7 @@ export function LoginView(props) {
   );
 }
 
-LoginView.PropTypes = {
+LoginView.propTypes = {
   user: PropTypes.shape({
     username: PropTypes.string.isRequired,
     password: PropTypes.string.isRequired
