3feb7baf465edcabaff058ffaaa79e74a9b00c9b
marjsky <67877013+marjsky@users.noreply.github.com>
2022-08-14 20:46:56 -0400

    add code in actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,7 +2,6 @@ export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 
 export function setMovies(value) {
-  console.log('SET_MOVIES action triggered');
   return { 
     type: SET_MOVIES, 
     value 
